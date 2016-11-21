d4a12e38eab196cebb03dcdc71cc7335e7b68808
Don Owens <don@regexguy.com>
2016-11-21 02:28:43 +0000

    got flattening working

--- ini/ini.go
+++ ini/ini.go
@@ -1,3 +1,36 @@
+// Copyright (c) 2016 Don Owens <don@regexguy.com>.  All rights reserved.
+//
+// This software is released under the BSD license:
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//
+//  * Neither the name of the author nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+// OF THE POSSIBILITY OF SUCH DAMAGE.
+
 package ini
 
 import (
@@ -8,22 +41,71 @@ import (
     "strings"
 )
 
+type my_ini struct {
+    foo string
+}
+
 func LoadFromFile(filename string) (map[string]map[string]string, error) {
+    o := new(my_ini)
+    return o.load_from_file(filename)
+}
+
+func LoadFromReader(r io.Reader) (map[string]map[string]string, error) {
+    o := new(my_ini)
+    return o.load_from_reader(r)
+}
+
+func LoadFromString(buf string) (map[string]map[string]string, error) {
+    o := new(my_ini)
+    return o.load_from_reader(strings.NewReader(buf))
+}
+
+func LoadFromFileFlat(filename string) (map[string]string, error) {
+    o := new(my_ini)
+
+    conf, err := o.load_from_file(filename)
+    if err != nil {
+        return nil, err
+    }
+
+    flat := o.structured_to_flat(conf, ".")
+    return flat, nil
+}
+
+func LoadFromReaderFlat(r io.Reader) (map[string]string, error) {
+    o := new(my_ini)
+    conf, err := o.load_from_reader(r)
+    if err != nil {
+        return nil, err
+    }
+
+    flat := o.structured_to_flat(conf, ".")
+    return flat, nil
+
+}
+
+func LoadFromStringFlat(buf string) (map[string]string, error) {
+    o := new(my_ini)
+    conf, err := o.load_from_reader(strings.NewReader(buf))
+    if err != nil {
+        return nil, err
+    }
+
+    flat := o.structured_to_flat(conf, ".")
+    return flat, nil
+}
+
+func (o *my_ini) load_from_file(filename string) (map[string]map[string]string, error) {
     f, err := os.Open(filename)
     if err != nil {
         return nil, nil
     }
     defer f.Close()
 
-    // return LoadFromReader(bufio.NewReader(f))
-    return LoadFromReader(f)
+    return o.load_from_reader(f)
 }
 
-func LoadFromString(buf string) (map[string]map[string]string, error) {
-    return LoadFromReader(strings.NewReader(buf))
-}
-
-func LoadFromReader(r io.Reader) (map[string]map[string]string, error) {
+func (o *my_ini) load_from_reader(r io.Reader) (map[string]map[string]string, error) {
     in := bufio.NewReader(r)
     conf := make(map[string]map[string]string)
     cur_sect_name := "default"
@@ -59,8 +141,12 @@ func LoadFromReader(r io.Reader) (map[string]map[string]string, error) {
                     cur_sect_name = string(bytes[1:i])
                     cur_sect = make(map[string]string)
                     conf[cur_sect_name] = cur_sect
+                    break
                 }
             }
+
+            // FIXME: if b[0] != ']' -- return error
+
         } else {
             parts := strings.SplitN(line, "=", 2)
             k, v := strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])
@@ -69,8 +155,17 @@ func LoadFromReader(r io.Reader) (map[string]map[string]string, error) {
         
     }
     
-
-    // conf["default"] = map[string]string{"foo":"bar","key1":"val1"}
-
     return conf, nil
+}
+
+func (o *my_ini) structured_to_flat(conf_in map[string]map[string]string, del string) map[string]string {
+    conf := make(map[string]string)
+    for sect_name, sect := range conf_in {
+        for k, v := range sect {
+            name := sect_name + del + k
+            conf[name] = v
+        }
+    }
+
+    return conf
 }
--- ini_test.go
+++ ini_test.go
@@ -20,9 +20,32 @@ func TestLoad(t *testing.T) {
     }
 
     if !reflect.DeepEqual(conf, expecting) {
-        t.Errorf("didn't get expected values from conf\n\tgot %v\n\texpected %v",
+        t.Errorf("didn't get expected values from structured conf\n\tgot %v\n\texpected %v",
             conf, expecting)
     }
 
+
     // t.Errorf("conf=%v", conf)
+}
+
+func TestFlatLoad(t *testing.T) {
+    buf := "foo=1\n\n; this is a comment\n[test]\nfoo=1\nbar=2\n[section1]\nfoo=bar\n"
+
+    flat_conf, err := ini.LoadFromStringFlat(buf)
+    if err != nil {
+        t.Error("LoadFromStringFlat() failed")
+    }
+
+    flat_expecting := map[string]string{
+        "default.foo":"1",
+        "test.foo":"1",
+        "test.bar":"2",
+        "section1.foo":"bar",
+    }
+
+    if !reflect.DeepEqual(flat_conf, flat_expecting) {
+        t.Errorf("didn't get expected values from flat conf\n\tgot %v\n\texpected %v",
+            flat_conf, flat_expecting)
+    }
+
 }
